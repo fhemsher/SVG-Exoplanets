cced0f62444d9587c0f719d2eaa30049ee63e6df
fhemsher <fhemsher@gmail.com>
2018-11-19 13:55:09 -0500

    Add files via upload

---
+++ Exoplanet/PlanetView/planetView.js
@@ -0,0 +1,127 @@
+var PlanetSVG
+var PlanetViewG
+var PlanetViewWidth
+var PlanetViewHeight
+function initPlanetMap()
+{
+    PlanetViewWidth=800
+    PlanetViewHeight=600
+     PlanetSVG = d3.select("#viewPlanetDiv").append("svg")
+    .attr("width", "100%").attr("height", "100%")
+    .attr("id", "planetSVG")
+    .attr("fill", "none")
+    .attr("viewBox", "0 0 "+PlanetViewWidth+" "+PlanetViewHeight)
+
+     PlanetViewG=PlanetSVG.append("svg")
+      .attr("id","planetViewG")
+
+
+    beginPlanetMap()
+
+
+
+
+
+
+
+
+}
+var PlanetProjection
+var PlanetViewMap
+var PlanetView
+var PlanetProjection
+var PlanetGrid
+var PlanetOutline
+var PlanetAtmos
+
+var StopPlanetZoom=false
+function beginPlanetMap()
+{
+
+    if(!PlanetProjection)
+    {
+
+        StopPlanetZoom = false
+        //---star view ditto celestial view
+
+         PlanetView = {r:[0, 0, 0], k: 140};
+        //---rotate and scale this to match celestial---
+        PlanetProjection = d3.geo.mollweide().rotate([0, 0, 0]).translate([PlanetViewWidth/2, PlanetViewHeight/2]).scale([140])
+        PlanetViewMap = d3.geo.path().projection(PlanetProjection);
+        //projOutline = d3.geo.mollweide().translate([PlanetViewWidth/2, PlanetViewHeight/2]).scale([140]); //projected non moving outline
+
+
+        //---creates a 'shadow' grid identical to celestial---
+        var graticule = d3.geo.graticule().minorStep([8, 4]);
+         //PlanetOutline = d3.geo.path().projection(projOutline)
+        PlanetAtmos=PlanetViewG.append("path").datum(graticule.outline).attr("id", "planetOutline").attr("d", PlanetViewMap);
+
+        PlanetGrid=PlanetViewG.append("path").datum(graticule).attr("id","planetGrid").attr("fill", "none").attr("stroke", "black").attr("stroke-width", ".5").attr("d", PlanetViewMap);
+
+        //---restructure zoom for higher scale performance, PAN in lieu of rotation---
+        PlanetZoom = d3.behavior.zoom().translate(PlanetProjection.translate()).scale(PlanetProjection.scale()).size([PlanetViewWidth, PlanetViewHeight]).scaleExtent([1, 100000]).on("zoom", planetRedraw);
+        PlanetSVG.call(PlanetZoom).on("dblclick.zoom", null)
+            PlanetAtmos.attr("fill", "none")
+            PlanetAtmos.attr("stroke", "#191970")
+            PlanetAtmos.attr("stroke-width", "6")
+
+        PlanetView.k = 140
+        PlanetView.r = [0,0,0]
+        PlanetProjection.scale(PlanetView.k)
+        PlanetProjection.rotate(PlanetView.r)
+        PlanetProjection.translate([PlanetViewWidth/2, PlanetViewHeight/2])
+        PlanetZoom.scale(PlanetView.k)
+    }
+    else
+    {
+        PlanetProjection.rotate([PlanetView.r[0], PlanetView.r[1], PlanetView.r[2]]).translate([PlanetViewWidth/2, PlanetViewHeight/2])
+        PlanetProjection.scale([PlanetView.k])
+
+    }
+
+}
+var PrevPlanetTransX=0
+var PrevPlanetTransY=0
+var PrevPlanetScale=140
+
+
+function planetRedraw()
+{
+    if(StopPlanetZoom==false)
+    {
+        if(d3.event )
+        {
+            starG.style.cursor = "default"
+            PlanetProjection.translate(d3.event.translate).rotate(PlanetProjection.rotate()).scale(d3.event.scale);
+
+        }
+        else
+        {
+
+            StarProjection.translate([PrevPlanetTransX, PrevPlanetTransY]).rotate(PlanetProjection.rotate()).scale(PrevPlanetScale);
+            PlanetZoom.translate([PrevPlanetTransX, PrevPlanetTransY])
+            PlanetZoom.scale(PrevPlanetScale)
+
+        }
+
+        //PlanetProjection.rotate(PlanetProjection.rotate()).scale(PlanetProjection.scale());
+
+
+        PlanetView =
+        {
+        r: PlanetProjection.rotate(), k: PlanetProjection.scale()
+        };
+
+        PlanetGrid.attr("d", PlanetViewMap);
+        d3.select("#planetOutline").attr("d", PlanetViewMap);
+
+        if(d3.event)
+        {
+            PrevPlanetScale = d3.event.scale
+            PrevPlanetTransX = d3.event.translate[0]
+            PrevPlanetTransY = d3.event.translate[1]
+        }
+
+    }
+
+}
---
+++ Exoplanet/PlanetView/viewPlanet.js
@@ -0,0 +1,78 @@
+var StopAnim=false
+function viewPlanetSelected()
+{
+
+    PlanetView.k = 140
+    PlanetView.r = [0,0,0]
+    PlanetProjection.scale(PlanetView.k)
+    PlanetProjection.rotate(PlanetView.r)
+   // PlanetProjection.translate([PlanetViewWidth/2, PlanetViewHeight/2])
+    PlanetZoom.scale(PlanetView.k)
+    PrevPlanetTransX=PlanetViewWidth/2
+    PrevPlanetTransY=PlanetViewHeight/2
+    PrevPlanetScale=140
+          PlanetProjection.rotate([PlanetView.r[0], PlanetView.r[1], PlanetView.r[2]]).translate([PlanetViewWidth/2, PlanetViewHeight/2])
+        PlanetProjection.scale([PlanetView.k])
+      planetRedraw()
+
+    planetId=viewPlanetSelect.options[viewPlanetSelect.selectedIndex].value
+   var planet=document.getElementById(planetId)
+   var planetColor=planet.getAttribute("planetColor")
+
+  // planetGrid.setAttribute("fill",planetColor)
+ //  if(planetColor!="white")
+  //  planetGrid.setAttribute("fill-opacity",.4)
+    planetOutline.setAttribute("stroke",planet.getAttribute("stroke"))
+    planetOutline.setAttribute("fill",planetColor)
+     if(planetColor!="white")
+    planetOutline.setAttribute("fill-opacity",.4)
+
+   var planetName= planet.getAttribute("planetName")
+   planetNameSpan.innerHTML= planetName
+
+ /*
+  var clonePlanet=planet.cloneNode(true)
+   clonePlanet.removeAttribute("id")
+   clonePlanet.removeAttribute("transform")
+   clonePlanet.removeAttribute("class")
+   clonePlanet.setAttribute("cx","50%")
+   clonePlanet.setAttribute("stroke-width","8")
+   clonePlanet.setAttribute("cy","50%")
+   clonePlanet.setAttribute("r","40%")
+   var planetName= clonePlanet.getAttribute("planetName")
+   planetNameSpan.innerHTML= planetName
+
+
+   var atmos=clonePlanet.getAttribute("atmos")
+   var comp=clonePlanet.getAttribute("comp")
+   var hab=clonePlanet.getAttribute("hab")
+   var data="<b>Atmoshpere:</b> "+ atmos+", <b>Composition:</b> "+comp+", <b>Habitable:</b> "+hab
+   planetDataSpan.innerHTML=data
+   //----replace fill with non-rotating----
+
+  var url=clonePlanet.getAttribute("hab")+"Stop"
+   clonePlanet.setAttribute("fill","url(#"+url+")")
+
+   viewSVG.appendChild(clonePlanet)
+ */
+
+   viewPlanetDiv.style.visibility="visible"
+   var height=600
+    d3.select("#viewPlanetDiv").transition().duration(800).style("height",height+"px")
+
+
+
+
+
+}
+
+
+
+function closeViewPlanet()
+{
+    var height=1
+
+         d3.select("#viewPlanetDiv").transition().duration(800).style("height",height+"px")
+        setTimeout('viewPlanetDiv.style.visibility="hidden"',1000)
+    viewPlanetSelect.selectedIndex=0
+}
